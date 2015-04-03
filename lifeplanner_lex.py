eedb7e1418e76e489275d3ed2c69ab927fe4a1b1
Katie Lin <kl2532@columbia.edu>
2015-04-02 22:39:48 -0400

    Added keywords

--- lifeplanner_lex.py
+++ lifeplanner_lex.py
@@ -5,8 +5,10 @@ import lex
 tokens = (
     'INTEGER','DECIMAL','CHARACTER', 'STRING', 
     'PLUS','MINUS','DIVIDE',
-    'TIMES', 'NEWLINE', 'LEFTPAREN',
-    'RIGHTPAREN', 'BUILD', 'PRINT', 'TAB', 'SCHEDULE')
+    'TIMES', 'NEWLINE', 'LEFTPAREN', 'COLON',
+    'RIGHTPAREN', 'BUILD', 'PRINT', 'TAB', 'SCHEDULE', 'EXPORT', 'IMPORT'
+    'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY', 'AM', 'PM',
+    'TO', 'WITH', 'FROM', 'AT', 'TAG')
 
 # ----REGULAR EXPRESSION PATTERNS---
 
@@ -23,17 +25,43 @@ t_PLUS       = r'\+'
 t_MINUS	     = r'\-'
 t_TIMES      = r'\*'
 t_DIVIDE    = r'/'
-t_TAB = r'\t'
+
+# Regular expression patterns
+# for whitespace
 t_NEWLINE = r'\n'
 
 # Regular expression patterns for multi-use
 # tokens.
-t_LEFTPAREN  = r'\('
-t_RIGHTPAREN = r'\)'
+
+# Keywords
+t_MONDAY = r'(Monday)'
+t_TUESDAY = r'(Tuesday)'
+t_WEDNESDAY = r'(Wednesday)'
+t_THURSDAY = r'(Thursday)'
+t_FRIDAY = r'(Friday)'
+t_SATURDAY = r'(Saturday)'
+t_SUNDAY = r'(Sunday)'
 t_BUILD = r'(build)'
 t_SCHEDULE = r'(schedule)'
 t_PRINT = r'(print)'
-t_ignore = r' '
+t_IMPORT = r'(import)'
+t_EXPORT = r'(export)'
+t_TAG = r'(tag)'
+t_AT = r'(at)'
+t_FROM = r'(from)'
+t_TO = r'(to)'
+t_WITH = r'(with)'
+
+# Time Meridian
+t_AM = r'(AM)'
+t_PM = r'(PM)'
+t_ignore = r' \t'
+
+# Punctuation
+t_LEFTPAREN  = r'\('
+t_RIGHTPAREN = r'\)'
+t_COLON = r'\:'
+
 
 # def t_NEWLINE(t):       # When a \n is found,
 #     r'\n'               # increment the line
