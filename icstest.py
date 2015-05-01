cb4ea898ebb0a3b52a305000d8d052bcf7cd2804
Katie Lin <kl2532@columbia.edu>
2015-05-01 17:40:55 -0400

    updated .py files about importing/exporting to .ics

--- icstest.py
+++ icstest.py
@@ -1,4 +1,3 @@
-#from ics import Calendar, Event
 import event as e
 import ourCalendar as c
 from datetime import datetime
@@ -6,19 +5,28 @@ from dateutil import tz
 
 #declaring a calendar
 cal = c.ourCalendar()
-begin = datetime.strptime('2015-01-01 02:30:40', '%Y-%m-%d %H:%M:%S')
-end = datetime.strptime('2015-01-01 03:00:40', '%Y-%m-%d %H:%M:%S')
-begin2 = datetime.strptime('2015-01-01 05:30:40', '%Y-%m-%d %H:%M:%S')
-end2 = datetime.strptime('2015-01-01 06:00:40', '%Y-%m-%d %H:%M:%S')
+# Read original Calendar
+orig_event_dict = cal.readCalendar('my.ics') # [{'name': ..., 'to': ..., 'from': ...,  'at':..., 'with':...}, {'name': ..., 'to': ..., 'from': ...,  'at':..., 'with':...}, ...]
+for orig_e in orig_event_dict:
+	e_name = orig_e['name']
+	e_to = orig_e['to']
+	e_from = orig_e['from']
+	e_at = orig_e['at']
+	e_with = orig_e['with']
+	orig_event = e.Event(e_name, e_from, e_to, e_at, e_with)
+	cal.addEvent(orig_event.create_string_event())
+
+#Event 1
+begin = datetime.strptime('2015-01-02 02:30:40', '%Y-%m-%d %H:%M:%S')
+end = datetime.strptime('2015-01-02 03:00:40', '%Y-%m-%d %H:%M:%S')
 event = e.Event("Wake", begin, end, "Hungarian", "Suna")
+cal.addEvent(event.create_string_event())
+# Event 2
+begin2 = datetime.strptime('2015-01-02 05:30:40', '%Y-%m-%d %H:%M:%S')
+end2 = datetime.strptime('2015-01-02 06:00:40', '%Y-%m-%d %H:%M:%S')
 event2 = e.Event("Wake2", begin2, end2, "Hungarian", "Suna")
-l = event.createEvent()
-l2 = event2.createEvent()
-cal.addEvent(l)
-cal.addEvent(l2)
-cal.write_file('my.ics')
-
-cal.readCalendar('my.ics')
-
+cal.addEvent(event2.create_string_event())
+# Write a .ics file
+cal.write_file('new.ics')
 
 # UID and DTSTAMP are unique for each event
--- ourCalendar.py
+++ ourCalendar.py
@@ -2,28 +2,30 @@ from datetime import datetime
 from dateutil import tz
 
 class ourCalendar:
-	"""Represents a calendar ics"""
+	"""Represents a calendar .ics"""
 
 	def __init__(self):
+		# string that will be written to .ics
 		self.events = "BEGIN:VCALENDAR\nVERSION:2.0\n"
 
-	def addEvent(self, event):
-		self.events+=event+"\n"
+	def add_event(self, event):
+		self.events += event + "\n"
 
 	def write_file(self, file_name):
-		# returns a string 
+		# writes to a .ics file
 		self.events+="END:VCALENDAR\n"
 		with open(file_name, 'w') as f:
 			f.write(self.events)
 
-	def readCalendar(self, orig_file):
+	def read_calendar(self, orig_file):
 		# returns a list of events where each event is a dictionary
-		stuff = [] #[{"BEGIN:VEVENT...END:VEVNT"}, {"BEGIN:VEVENT...END:VEVNT"}, ...]
-		i = 0
+		# example: [{'name': ..., 'to': ..., 'from': ...,  'at':..., 'with':...}, {'name': ..., 'to': ..., 'from': ...,  'at':..., 'with':...}, ...]
+		event_list = [] 
 		with open(orig_file, 'r') as f:
+			i = 0
 			for line in f:
 				if(line.startswith('BEGIN:VEVENT')):
-					stuff.append({})
+					event_list.append({})
 				if(line.startswith('DTSTART')):
 					info = line.split(":")
 					time = datetime.strptime(info[1].rstrip(), '%Y%m%dT%H%M%SZ')
@@ -31,7 +33,7 @@ class ourCalendar:
 					to_zone = tz.tzlocal()
 					time = time.replace(tzinfo=from_zone)
 					time = time.astimezone(to_zone)
-					stuff[i]["from"] = time
+					event_list[i]["from"] = time
 				if(line.startswith('DTEND')):
 					info = line.split(":")
 					time = datetime.strptime(info[1].rstrip(), '%Y%m%dT%H%M%SZ')
@@ -39,23 +41,22 @@ class ourCalendar:
 					to_zone = tz.tzlocal()
 					time = time.replace(tzinfo=from_zone)
 					time = time.astimezone(to_zone)
-					stuff[i]["to"] = time
-				if(line.startswith('with')):
+					event_list[i]["to"] = time
+				if(line.startswith('DESCRIPTION')):
 					info = line.split(":")
-					if(info[1].rstrip().startswith('with')):
-						stuff[i]["DESCRIPTION"] = info[1].rstrip()
+					if(info[1].rstrip()):
+						event_list[i]["with"] = info[1].rstrip()
 					else:
-						stuff[i]["DESCRIPTION"] = ""
+						event_list[i]["with"] = ""
 				if(line.startswith('LOCATION')):
 					info = line.split(":")
-					stuff[i]["at"] = info[1].rstrip()
+					event_list[i]["at"] = info[1].rstrip()
 				if(line.startswith('SUMMARY')):
 					info = line.split(":")
-					stuff[i]["name"] = info[1].rstrip()
+					event_list[i]["name"] = info[1].rstrip()
 				if(line.startswith('END:VEVENT')):
 					i = i+1
-		print stuff
-		return stuff
+		return event_list
 
 
 
