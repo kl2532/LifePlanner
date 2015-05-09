408c3d9c3a5a08f163d8471a36832841f55fa113
Sean Liu <sean.liu@columbia.edu>
2015-05-09 14:00:37 -0400

    function blox

--- lifeplanner_translate.py
+++ lifeplanner_translate.py
@@ -51,12 +51,26 @@ def parse_program(tree, num_tabs):
 		entire_prog += dir_to_func['export_stmt'](tree[4][1:], num_tabs)
 	return entire_prog
 
+# [['function_block', ['function_declaration', 'function', ['function_name', 'a'], ['parameter_list', None]], ['expr_block', ['expr', ['assignment_stmt', ['variable', 'x'], '=', ['value', ['math_int', '1']]]], ['expr_block_rep', ['expr_block', None]]], ['return_stmt', 'return', ['value', ['math_var', ['variable', 'x']]]]], ['function_block', ['function_declaration', 'function', ['function_name', 'b'], ['parameter_list', 'x', ['parameter_list', 'y', ['parameter_list', 'z', ['parameter_list', None]]]]], ['expr_block', ['expr', ['print_stmt', ['print', 'print'], ['quote', '"'], ['strings', 'hello'], ['quote', '"']]], ['expr_block_rep', ['expr_block', None]]], ['return_stmt', None]]]
+
 def parse_function_blocks(tree, num_tabs):
-	return 'function_blocks'
-	#need to finish translating
+	print tree
+	code = ''
+	for f_block in tree:
+		if f_block == None:
+			return code
+		code += parse_function_block(f_block[1:], num_tabs)
+	return code
+
+
+# [['function_declaration', 'function', ['function_name', 'f'], ['parameter_list', None]], ['expr_block', ['expr', ['assignment_stmt', ['variable', 'x'], '=', ['value', ['math_int', '1']]]], ['expr_block_rep', ['expr_block', None]]], ['return_stmt', 'return', ['value', ['math_var', ['variable', 'x']]]]]
 
 def parse_function_block(tree, num_tabs):
-	pass
+	# print tree[1][1:]
+	code = parse_function_declaration(tree[0][2:], num_tabs)
+	code += parse_expr_block(tree[1][1:], num_tabs+1)
+	code += parse_return_stmt(tree[2][1:], num_tabs+1)
+	return code
 
 def parse_function_declaration(tree, num_tabs):
 	code = ''
@@ -66,12 +80,24 @@ def parse_function_declaration(tree, num_tabs):
 		i += 1
 	code += 'def ' + dir_to_func['variable'](tree[0][1], num_tabs) + '('
 	params = dir_to_func['parameter_list'](tree[1], num_tabs)
-	x = params.split()
-	params = x[0]
-	for i in range(1, len(x)):
-		params += ', ' + x[i]
+	if params != '':
+		x = params.split()
+		params = x[0]
+		for i in range(1, len(x)):
+			params += ', ' + x[i]
 	code += params + '):\n'
 	return code
+
+
+def parse_return_stmt(tree, num_tabs):
+	if not tree[0]:
+		return '\n\n'
+	code = ''
+	for i in range(num_tabs):
+		code += '\t'
+	code += 'return ' + parse_value(tree[1][1], num_tabs) + '\n\n'
+	return code
+
 
 def parse_import_stmt(tree, num_tabs):
 	code = 'orig_event_dict = cal.read_calendar(\'' + str(dir_to_func['filename'](tree[1][1:], num_tabs)) + '\')\n'
@@ -118,7 +144,7 @@ def parse_schedule_stmts_rep(tree, num_tabs):
 	return ''
 
 def parse_build_schedule(tree, num_tabs):
-	print "parse_build_schedule tree: ", tree
+	# print "parse_build_schedule tree: ", tree
 	# return "hello"
 	if len(tree) != 4:
 		sys.stderr.write('Invalid build schedule')
@@ -174,7 +200,7 @@ def parse_expr_block_rep(tree, num_tabs):
 	return ''
 
 def parse_expr(tree, num_tabs):
-	print 'parse_expr tree: ', str(tree)
+	# print 'parse_expr tree: ', str(tree)
 	code = ''
 	#done
 	if tree[0][0] == 'print_stmt':
@@ -228,7 +254,7 @@ def parse_func(tree, num_tabs):
 	return code
 
 def parse_parameter_list(tree, num_tabs):
-	print '\nparams: ' + str(tree)
+	# print '\nparams: ' + str(tree)
 	params = ''
 	if tree[1]:
 		params += tree[1]
@@ -239,7 +265,7 @@ def parse_day_math(tree, num_tabs):
 	pass
 
 def parse_time_math(tree, num_tabs):
-	print 'parse_time_math: ', str(tree)
+	# print 'parse_time_math: ', str(tree)
 	code = ''
 	if tree[0][0] == 'time':
 		hour, minute = dir_to_func['time_elements'](tree[0][1:], num_tabs)
@@ -264,7 +290,7 @@ def parse_op(tree, num_tabs):
 	return tree[0]
 
 def parse_event_stmt(tree, num_tabs):
-	print "parse_event_stmt: ", str(tree)
+	# print "parse_event_stmt: ", str(tree)
 	if tree[0][0] == 'cancel_stmt':
 		return dir_to_func['cancel_stmt'](tree[0][1:], num_tabs)
 	if tree[0][0] == 'update_stmt':
@@ -341,7 +367,7 @@ def parse_assignment_stmt(tree, num_tabs):
 	return code
 
 def parse_math_stmt(tree, num_tabs):
-	print 'math tree: ', tree
+	# print 'math tree: ', tree
 	label = tree[0]
 	if len(tree) == 4:
 		first = tree[1]
@@ -379,18 +405,18 @@ def parse_variable(tree, num_tabs):
 	return str(tree)
 
 def parse_value(tree, num_tabs):
-	print 'parse_value tree: ' + str(tree)
 	label = tree[0]
+	# print 'LABEL:', label[0:4]
 	if label in \
 	['variable', 'num', 'time', 'date', 'event', 'tag', 'time_math', 'access']:
 		return dir_to_func[label](tree[1:], num_tabs)
-	if 'math_' in label:
-		return dir_to_func['math_stmt'](tree[:], num_tabs)
+	if label[0:4] == 'math':
+		return dir_to_func['math_stmt'](tree, num_tabs)
 	else:
 		return 'in parse_value'
 
 def parse_access(tree, num_tabs):
-	print '\nparse_access: ' + str(tree)
+	# print '\nparse_access: ' + str(tree)
 	code = 'get_event(\'' + dir_to_func['strings'](tree[0][1], num_tabs) + '\', var_all_events)[\'' + tree[1] + '\']'
 	return code
 
@@ -454,10 +480,10 @@ def parse_date(tree, num_tabs):
 
 def parse_event_list(tree, num_tabs, month, day, year):
 	# if len(tree) != 2:
-	# 	return -1
-	print
-	print 'event' + str(tree)
-	print
+	# # 	return -1
+	# print
+	# print 'event' + str(tree)
+	# print
 	if not tree[0]:
 		return ''
 	if tree[0][0] != 'event':
@@ -471,11 +497,11 @@ def parse_event_list(tree, num_tabs, month, day, year):
 	code + '\n' 
 
 def parse_event_list_rep(tree, num_tabs, month, day, year=None):
-	print tree
-	print
+	# print tree
+	# print
 	if tree and tree[0][0] == 'event_list':
-		print tree[0][1:]
-		print
+		# print tree[0][1:]
+		# print
 		return dir_to_func['event_list'](tree[0][1:], num_tabs, month, day, year)
 	return ''
 
@@ -786,9 +812,6 @@ def parse_for_stmt(tree, num_tabs):
 	
 def parse_date_duration(tree, num_tabs):
 	pass
-			
-def parse_return_stmt(tree, num_tabs):
-	pass
 	
 def parse_and(tree, num_tabs):
 	pass
@@ -801,9 +824,7 @@ def parse_tag_priorities(tree, num_tabs):
 	
 def parse_comment_stmt(tree, num_tabs):
 	return ''
-	
-def parse_function_declaration(tree, num_tabs):
-	pass
+
 
 def parse_string(tree, num_tabs):
 	pass
