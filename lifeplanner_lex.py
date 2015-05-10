8353131c7e8038ddc2e13e391da4c6d61f05163e
Rona (Yanrong Wo) <yw2513@columbia.edu>
2015-05-10 16:51:54 -0400

    Updated the regular expression for user string again

--- lifeplanner_lex.py
+++ lifeplanner_lex.py
@@ -162,7 +162,7 @@ t_DECIMAL   = r'[\-]?[0-9]+\.[0-9]*'
 # over the reserved words, string has no quotes and is
 # not prefered over the reserved words
 def t_USERSTRING(t):
-    r'\"[^\""]+\"'
+    r'\"[^\"]*\"'
     return t
 
 def t_STRING(t):
